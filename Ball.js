6f0cc044179c5acefc1cf51d23b42918f0700002
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-03 15:43:04 +0500

    Started Ball JS
    add getters and setters for ball position i.e x and y

--- Ball.js
+++ Ball.js
@@ -2,4 +2,20 @@ export default class Ball {
   constructor(ballElem) {
     this.ballElem = ballElem;
   };
+
+  get x() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
+  };
+
+  set x(value) {
+    this.ballElem.style.setProperty("--x", value);
+  };
+
+  get y() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"));
+  };
+
+  set y(value) {
+    this.ballElem.style.setProperty("--y", value);
+  };
 };
